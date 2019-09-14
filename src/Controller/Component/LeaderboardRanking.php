cb2e2b37a098c5c5aab08e988fc3cfd1eb25ef16
Desarrollo <rguerrero@c4-technologies.com>
2019-09-13 17:02:24 -0700

    loaderborad users

--- src/Controller/Component/LeaderboardRanking.php
+++ src/Controller/Component/LeaderboardRanking.php
@@ -26,13 +26,9 @@ class LeaderboardRanking
     public $competition_id;
     public $matches_list;
     public $usersId_list = [];
+    public $usersId_list_index = [];
     public $users_list = [];
-    public $accounts_period;
-    public $current_period;
-    public $monthly_payments;
-    public $agents;
-    public $start_date;
-    public $end_date;
+    public $position_count = 1;
 
     public function __construct($data = [])
     {
@@ -40,6 +36,7 @@ class LeaderboardRanking
         $this->Competitions =  TableRegistry::get('Competitions');
         $this->Matches =  TableRegistry::get('Matches');
         $this->MatchesUsers = TableRegistry::get('MatchesUsers');
+        $this->Crews = TableRegistry::get('Crews');
         $this->validateArgs($data);
 
         $this->season = $data['season'];
@@ -71,20 +68,12 @@ class LeaderboardRanking
         $this->competition_id = $this->Competitions->find()->where(['slug' => $this->competition])->first();
         $this->matches_list = $this->getMatches($this->competition_id->id);
         foreach ($this->matches_list as $match) {
-            $this->getUsersIdsByMatches($match->id);
+            $this->getUsersIdsByMatches($match->id, $match->points);
         }
         //limpiando ids de usuarios
-        $this->usersId_list = array_unique($this->usersId_list);
-        $this->usersId_list= array_diff($this->usersId_list, ['']); 
-        $this->getUsers($this->usersId_list);
-        //show ids from users
-        echo implode(',',$this->usersId_list);
-        echo('<br>');
-        foreach ($this->usersId_list as $users) {
-            echo($users);
-            echo('<br>');
-        }
-
+        $this->wipeUsersByPoints();
+        //work here
+        $this->getData();
     }
 
     private function getMatches($competition_id)
@@ -92,22 +81,89 @@ class LeaderboardRanking
         return $this->Matches->find()->where(['competition_id' => $competition_id]);
     }
 
-    public function getUsersIdsByMatches($matches_id)
+    public function getUsersIdsByMatches($matches_id, $points)
     {
-        $users = $this->MatchesUsers->find()->where(['match_id'=>$matches_id]);
+        $users = $this->MatchesUsers->find()->where(['match_id' => $matches_id]);
         foreach ($users as $user) {
-            $this->usersId_list[]=$user->user_id;     
+            if (array_key_exists($points, $this->usersId_list)) {
+                $usersTmp = $this->usersId_list[$points];
+            }
+            $usersTmp[] =  $user->user_id;
+            $this->usersId_list[$points] = $usersTmp;
+        }
+        $this->usersId_list_index[] = $points;
+    }
+
+    public function wipeUsersByPoints()
+    {
+        ksort($this->usersId_list);
+        $this->usersId_list = array_reverse($this->usersId_list, true);
+        //wipe index by points, and order by more hit
+        $this->usersId_list_index = array_unique($this->usersId_list_index);
+        sort($this->usersId_list_index);
+        $this->usersId_list_index = array_reverse($this->usersId_list_index, true);
+
+        //wipe repety users by keys
+        $wipeTmp;
+        foreach ($this->usersId_list as $key => $value) {
+            if (empty($wipeTmp)) {
+                $wipeTmp  = $value;
+            } else {
+                $tmp = $value;
+                $this->usersId_list[$key] = array_diff($value, $wipeTmp);
+                $wipeTmp  = $tmp;
+            }
         }
     }
 
-    public function getUsers($usersId)
+    public function getUser($users_id)
     {
-        $users = $this->MatchesUsers->Users->find()->where(['id IN'=>$usersId]);
-        foreach ($users as $user) {
-            echo($user->aka);
-            $this->users_list[]=$user->user_id;     
+        $users = $this->MatchesUsers->Users->get()->where(['id IN' => $users_id]);
+        return $users;
+    }
+    public function getCrew($crew_id)
+    {
+        if ($crew_id != 0 && $crew_id != null) {
+            $crew = $this->Crews->get($crew_id);
+            return $crew->name;
+        } else {
+            return null;
         }
+    }
+
+    public function getData()
+    {
+        //get data by keys
+        $dataTmp = [];
+        foreach ($this->usersId_list as $users_data) {
+            echo implode(',', $users_data);
+            echo ('<br>');
+        }
+        var_dump(array_keys($this->usersId_list));
         die();
+        //i stop here
+        foreach ($this->usersId_list as $key => $values) {
+            echo ($values);
+            echo ($key);
+            foreach ($values as $value) { 
+                $this->data = $this->getRow($value,$key);
+            }
+        }
+    }
+
+    public function getRow($users_id, $points)
+    {
+        //Posicion Freestyler Avatar  Crew Puntos
+        $userTmp = getUser($users_id);
+        $crewTmp = getCrew($userTmp->crew_id);
+        $rowTmp = [];
+        $rowTmp['Position'] = $this->position_count;
+        $rowTmp['Aka'] = $userTmp->aka;
+        $rowTmp['Avatar'] = $userTmp->avatar;
+        $rowTmp['Crew'] = $crewTmp ? $crewTmp : 'NA';
+        $rowTmp['Points'] = $points;
+        $this->position_count++;
+        return $rowTmp;
     }
 
     private function validateArgs($data = [])
