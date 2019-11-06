2209bf768f5572c5cb9c24d8aa722703ff7b6fde
Desarrollo <rguerrero@c4-technologies.com>
2019-11-06 10:38:23 -0800

    show 50 leagues per page

--- src/Controller/LeaguesController.php
+++ src/Controller/LeaguesController.php
@@ -20,16 +20,21 @@ class LeaguesController extends AppController
      */
     public function initialize()
     {
+        parent::initialize();
         $this->loadComponent('Flash');
         $this->loadComponent('Auth', [
             'authorize' => ['Controller'] // Added this line
         ]);
+        $this->loadComponent('Paginator');
     }
+
+    public $paginate = [
+        'limit' => 50
+    ];
 
     public function index()
     {
         $this->viewBuilder()->layout('deejee');
-        
         $leagues = $this->paginate($this->Leagues->find('all', [
             'order' => ['Leagues.name' => 'ASC']
         ]));
