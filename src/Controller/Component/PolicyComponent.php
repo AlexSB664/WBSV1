0c6d7b64a8b4f0cdda476c814a1410139f3d5ffb
Desarrollo <asilva@c4-technologies.com>
2020-03-11 17:01:37 -0700

    PolicyComponent complete

--- src/Controller/Component/PolicyComponent.php
+++ src/Controller/Component/PolicyComponent.php
@@ -15,6 +15,7 @@ class PolicyComponent extends Component
     {
         $this->LeaguesUsers = TableRegistry::get('LeaguesUsers');
         $this->Competitions = TableRegistry::get('Competitions');
+        $this->Seasons = TableRegistry::get('Seasons');
     }
     /**
      * Default configuration.
@@ -25,31 +26,60 @@ class PolicyComponent extends Component
 
     public function organizerPolicies($data = [])
     {
-        $user = $this->LeaguesUsers->Users->get($data['user']);
-        $leagues = $this->LeaguesUsers->getLeaguesByUser($user->id);
-        debug($data);
-        if (isset($data['competition'])) {
-            echo 'este';
-            return false;
+        $this->controller = $data['controller'];
+        $user = $this->LeaguesUsers->Users->get($this->controller->request->getSession()->read('Auth.User.id'));
+        if ($user->role == 'organizers') {
+            $this->controller = $data['controller'];
+            $this->action = $data['action'];
+            $leagues = $this->LeaguesUsers->getLeaguesByUser($user->id);
+
+            if ($data['competition']) {
+                $this->reject(
+                    $this->competitionManage($data['competition'], $leagues),
+                    'You do not have permissions to view this competition'
+                );
+            } elseif ($data['season']) {
+                $this->reject(
+                    $this->seasonManage($data['season'], $leagues),
+                    'You do not have permissions to view this season'
+                );
+            } elseif ($data['league']) {
+                $this->reject(
+                    $this->leagueManage($data['league'], $leagues),
+                    'You do not have permissions to view this league'
+                );
+            }
         }
-        if ($data['season']) {
-            echo 'este2';
-            return false;
-        }
-        if ($data['league']) {
-            echo 'este3';
-            return false;
-        }
-        die();
     }
 
-    public function leagueManage()
+    public function leagueManage($league, $leagues)
     {
+        return in_array($league, $leagues);
     }
-    public function seasonManage()
+    public function seasonManage($season, $leagues)
     {
+        return in_array(
+            $this->Seasons->get($season, [
+                'contain' => ['Leagues']
+            ])->league->id,
+            $leagues
+        );
     }
-    public function competitionManage()
+    public function competitionManage($competition, $leagues)
     {
+        return in_array(
+            $this->Competitions->get($competition, [
+                'contain' => ['Seasons.Leagues']
+            ])->season->league->id,
+            $leagues
+        );
+    }
+
+    public function reject($condition, $message)
+    {
+        if (!$condition) {
+            $this->controller->Flash->error(__($message));
+            return $this->controller->redirect(['action' => $this->action]);
+        }
     }
 }
